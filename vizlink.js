0b48f6a6456d11642c4662ddf6c8d35a41979366
Marcin Grzybowski <marcinjangrzybowski@gmail.com>
2023-05-15 02:59:46 +0200

    initial, dirty

---
+++ vizlink.js
@@ -0,0 +1,91 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+   var getSource = function(callback) {
+       var xhr = new XMLHttpRequest();
+       xhr.open('GET', "../", true);
+       xhr.responseType = 'text/html ';
+       xhr.onload = function() {
+	 var status = xhr.status;
+	 if (status === 200) {
+	   callback(null, xhr.response);
+	 } else {
+	   callback(status, xhr.response);
+	 }
+       };
+       xhr.send();
+   };
+
+    getSource(function(e,indexSource){
+    
+    
+    let moduleName = window.location.pathname.split("/").pop().slice(0,-5);
+	const defLinks = [];
+
+	let vizFrame = document.createElement("iframe");
+	
+    document.querySelectorAll("a.Function").forEach(
+	function(elem){
+	    if(elem.id == elem.href.split("#").pop() ){
+
+                defName = elem.innerText.trim();
+		    // slideSquareFaces
+		if(["slideSquare","∙v≡∙v'","_∙v'_","_∙v_"].includes(defName)){
+		    defName = "_." + defName;
+		}
+		// elem.style.color = ";
+		let vizLink = moduleName + "." + (defName);
+		let isIn = indexSource.includes(
+		    vizLink
+			.replaceAll("∙","&#x2219;")
+		        .replaceAll("₂","&#x2082;")
+		        .replaceAll("π","&#x3C0;")
+		        .replaceAll("₃","&#x2083;")
+		        .replaceAll("²","&#xB2;")
+		        .replaceAll("≡","&#x2261;")
+		        .replaceAll("'","&#x27;")
+		        .replaceAll("→","&#x2192;")
+		        .replaceAll("Ω","&#x3A9;")
+		    
+			+ ".json"
+		);
+		console.log(vizLink);
+                if(isIn){
+		    let urlLink =  "../web/#"+vizLink;
+		    let frameLink =  "../web/embed.html#"+vizLink;
+                    elem.href = urlLink;
+		    defLinks.push(elem);
+		    let vizIcon = document.createElement("div");
+		    vizIcon.classList.add("vizicon");
+		    elem.classList.add("vizlink");
+		    elem.parentNode.insertBefore(vizIcon,elem);
+		    elem.style.textDecoration = "underline";
+
+		    elem.href = "#";
+		    elem.addEventListener("click", function(e){
+                        vizFrame.src = frameLink;
+			vizFrame.style.width = "400px";
+			vizFrame.style.height = "400px";
+			vizFrame.style.display = "block";
+			elem.parentNode.insertBefore(vizFrame,elem);
+			e.preventDefault();
+			
+		    });
+
+		    let linkNewW = document.createElement("a");
+		    linkNewW.innerHTML = " (open)"
+                    linkNewW.href = urlLink;
+
+		    elem.insertAdjacentElement('afterend',linkNewW);
+		}
+		
+		
+		// console.log(vizLink)
+	    }
+	    // return true;
+	}
+    );
+    
+	console.log(defLinks.length);
+	});
+});
