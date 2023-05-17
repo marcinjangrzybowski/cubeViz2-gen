34e0b8510baf0aa9d2e75504e29a8b45fbc41860
Marcin Grzybowski <marcinjangrzybowski@gmail.com>
2023-05-18 00:22:23 +0200

    wip

--- vizlink.js
+++ vizlink.js
@@ -3,8 +3,8 @@
 document.addEventListener("DOMContentLoaded", function() {
    var getSource = function(callback) {
        var xhr = new XMLHttpRequest();
-       xhr.open('GET', "../", true);
-       xhr.responseType = 'text/html ';
+       xhr.open('GET', "vizIndex.json", true);
+       xhr.responseType = 'json';
        xhr.onload = function() {
 	 var status = xhr.status;
 	 if (status === 200) {
@@ -16,12 +16,14 @@ document.addEventListener("DOMContentLoaded", function() {
        xhr.send();
    };
 
-    getSource(function(e,indexSource){
+    getSource(function(e,vizIndex){
     
-    
+    // mVizIndex = "" 
+	
     let moduleName = window.location.pathname.split("/").pop().slice(0,-5);
 	const defLinks = [];
-
+        const mVizIndex = vizIndex[moduleName];
+	console.log(mVizIndex);
 	let vizFrame = document.createElement("iframe");
 	
     document.querySelectorAll("a.Function").forEach(
@@ -35,21 +37,24 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 		// elem.style.color = ";
 		let vizLink = moduleName + "." + (defName);
-		let isIn = indexSource.includes(
-		    vizLink
-			.replaceAll("∙","&#x2219;")
-		        .replaceAll("₂","&#x2082;")
-		        .replaceAll("π","&#x3C0;")
-		        .replaceAll("₃","&#x2083;")
-		        .replaceAll("²","&#xB2;")
-		        .replaceAll("≡","&#x2261;")
-		        .replaceAll("'","&#x27;")
-		        .replaceAll("→","&#x2192;")
-		        .replaceAll("Ω","&#x3A9;")
+		let isIn = mVizIndex.includes(vizLink + ".json");
 		    
-			+ ".json"
-		);
-		console.log(vizLink);
+		//       indexSource.includes(
+		//     vizLink
+		// 	.replaceAll("∙","&#x2219;")
+		//         .replaceAll("₂","&#x2082;")
+		//         .replaceAll("π","&#x3C0;")
+		//         .replaceAll("₃","&#x2083;")
+		//         .replaceAll("¹","&#xB9;")
+		//         .replaceAll("²","&#xB2;")
+		//         .replaceAll("≡","&#x2261;")
+		//         .replaceAll("'","&#x27;")
+		//         .replaceAll("→","&#x2192;")
+		//         .replaceAll("Ω","&#x3A9;")
+		    
+		// 	+ ".json"
+		// );
+		// console.log(defName);
                 if(isIn){
 		    let urlLink =  "../web/#"+vizLink;
 		    let frameLink =  "../web/embed.html#"+vizLink;
